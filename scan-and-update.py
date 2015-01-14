3903a1f7dfdc31922df074c259a19c183c5c2eea
Joe Corneli <holtzermann17@gmail.com>
2015-01-14 17:36:19 +0000

    Add code to scan and update from the distributed collection of URLs

---
+++ scan-and-update.py
@@ -0,0 +1,98 @@
+import os, sys
+import subprocess
+import filecmp
+import urllib
+
+from hammock import Hammock as Github
+import json
+import base64
+import re
+
+# Make sure to update the repo's contents before we start
+# Otherwise comparisons won't make sense.
+# Note, need to make sure that the cron user has permissions, shouldn't
+# be a problem because I've added the script to the crontab for "joe"
+# and joe has access to the /bibserver/ directory.
+p = subprocess.Popen(["git", "pull"],cwd="/bibserver/ims_legacy/BibProject/")
+p.wait()
+
+# A checked out version of miniBibServer
+lib_path = os.path.abspath('/bibserver/ims_legacy/miniBibServer/')
+sys.path.append(lib_path)
+
+# Subdirectory containing functions for doing the distributed update operation
+lib_path = os.path.abspath('/bibserver/ims_legacy/miniBibServer/github_api_stuff/')
+sys.path.append(lib_path)
+
+# A checked out version of the BibProject
+lib_path = os.path.abspath('/bibserver/ims_legacy/BibProject/')
+sys.path.append(lib_path)
+
+import ims_legacy
+import distributed_update
+
+# This file will contain the list of names and URLs of source material.
+# (It needs to be maintained by the IMS editor.)
+import ims_sources
+
+from pprint import pprint
+
+## Now go through this script, which works as follows:
+
+## Step 1: download copies of all files in our list
+
+## Step 2: compare downloaded files to local copies
+
+for item in ims_sources.urllist:
+    person_name=item[0]
+    # print "person name: " + person_name
+    source=item[1]
+    # Maybe the only thing to do in this case is produce some new HTML,
+    # assuming we keep "staged" and "working" copies separate.  But as
+    # a first step, let's assume there's nothing to do in this case, and
+    # we just need to process the upstream files.
+    if (source[:9] == "tex_files"):
+        upstream_filename = urllib.unquote(source.rsplit("/",1)[1])
+        # print "filename:" + upstream_filename
+        distributed_update.check_local(source)
+    else:
+        upstream_filename = urllib.unquote(source.rsplit("/",1)[1])
+
+        try: 
+            content = urllib.urlopen(source).read()
+        except urllib2.HTTPError, e:
+            # we could do some more sophisticated logging here, and throughout
+            print('HTTPError = ' + str(e.code))
+        else:
+            local_filename = "/bibserver/ims_legacy/BibProject/tex_files/" + person_name + ".tex"
+            new_filename = person_name + ".tex"
+            with open(new_filename, "w") as new_file:
+                # the comparison won't work unless the file has been written and closed!
+                new_file.write(content)
+                new_file.close()
+            # val will be True if the files are the same, and False if they differ.
+            val = filecmp.cmp(person_name + ".tex", local_filename)
+            #print "local filename:" + local_filename
+            #print "new filename:" + new_filename
+            #print "content:" + content[:36]
+            if (not(val)):
+                print new_filename + " differs."
+                basename = re.sub('[ ,.]', '', person_name)
+                distributed_update.make_branch(basename)
+                distributed_update.make_commit(basename,"tex_files",new_filename,content)
+
+                # we should probably update the HTML automatically as well, but maybe
+                # just what we do about it will depend on what IMS wants...
+
+                # I guess that part can wait until after deploying this version,
+                # it's not going to require a major change to the code.
+
+                # Something like this:
+                htmlcontent = ims_legacy.make_one("/bibserver/ims_legacy/"+new_filename)
+                htmlname = unicode(new_filename.split('.tex',1)[0],'utf-8')+".html"
+                distributed_update.make_commit(basename,"new_html_files",htmlname,htmlcontent)
+                distributed_update.make_pull_request(new_filename,basename)
+            else:
+                sys.stdout.write('.')
+
+print "done."
